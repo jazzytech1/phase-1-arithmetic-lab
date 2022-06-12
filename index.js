fe972e5395e8338c78175bfb437d3284ea782283
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-06-12 00:01:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2 
+
+const random = 1;
+Math.random > 0
+
+const num3 = 24;
+const num4 = 5;
+let mod = num3 % num4
+
+const max = 20;
+Math.max(1,2,3,4,20);
